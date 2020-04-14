360ecc69f5399c0454cb2d6693f590b1c5b333d7
Jolyon Russ <jolyonruss@gmail.com>
2020-04-14 08:06:48 -0400

    Bump the footer down

--- src/components/layout.js
+++ src/components/layout.js
@@ -58,7 +58,8 @@ const Layout = ({ location, title, children }) => {
     >
       <header>{header}</header>
       <main>{children}</main>
-      <footer>
+      <footer
+        style={{ marginTop: rhythm(3) }}>
         © {new Date().getFullYear()}, Built with
         {` `}
         <a href="https://www.gatsbyjs.org">Gatsby</a>
