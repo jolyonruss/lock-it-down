e48bdfca677c37665964183a30bf63925d69346e
Jolyon Russ <jolyonruss@gmail.com>
2020-04-18 23:59:58 -0400

    Adds tracking ID

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,7 +54,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-26468644-2`,
       }
     },
     `gatsby-plugin-feed`,
